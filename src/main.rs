25b60045625dbdf23b60d96a96b52e075f46c885
Berger Benjamin <benjamin.berger@byteforge.ch>
2025-04-11 00:00:54 +0200

    Cleanup variable-names and other compiler notes.

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,17 @@ use crate::employee::Employee;
 mod file_handler;
 use file_handler::FileHandler;
 
-use rand::thread_rng;
 use rand::Rng;
+use rand::thread_rng;
 
 fn main() {
-    let mut outputFileHandler = FileHandler::new();
+    let mut output_file_handler = FileHandler::new();
 
-    let file_path = get_file_path(String::from("input.json"));
-    // let file_path = "/Users/bberger/Code/walkinplanner/src/input.json";
+    // let file_path = get_file_path(String::from("input.json"));
+    let file_path = "/Users/bberger/Code/walkinplanner/src/input.json";
 
-    let mut daysPlanned: i128 = 0;
-    let mut daysWeekendAndHolidays: i128 = 0;
+    let mut days_planned: i128 = 0;
+    let mut days_weekend_and_holidays: i128 = 0;
     let mut errors = 0;
 
     // Read the file and parse the content
@@ -35,7 +35,7 @@ fn main() {
             // Extract employees from company_data
             let mut employees = company_data.employees;
             // Parse holidays into Vec<NaiveDate>
-            let globalHolidays: Vec<NaiveDate> = company_data
+            let global_holidays: Vec<NaiveDate> = company_data
                 .global_holidays
                 .iter()
                 .map(|s| NaiveDate::parse_from_str(s, "%d.%m.%Y"))
@@ -46,10 +46,10 @@ fn main() {
                 });
 
             // Parse the end date string into a NaiveDate
-            let mut currentDate: NaiveDate;
-            let startDate_string = &company_data.from;
-            match NaiveDate::parse_from_str(startDate_string, "%d.%m.%Y") {
-                Ok(date) => currentDate = date,
+            let mut current_date: NaiveDate;
+            let start_date_string = &company_data.from;
+            match NaiveDate::parse_from_str(start_date_string, "%d.%m.%Y") {
+                Ok(date) => current_date = date,
                 Err(e) => {
                     println!("Error parsing end date: {}", e);
                     return;
@@ -57,10 +57,10 @@ fn main() {
             }
 
             // Parse the start date string into a NaiveDate
-            let endDate: NaiveDate;
-            let endDate_string = &company_data.to;
-            match NaiveDate::parse_from_str(endDate_string, "%d.%m.%Y") {
-                Ok(date) => endDate = date,
+            let end_date: NaiveDate;
+            let end_date_string = &company_data.to;
+            match NaiveDate::parse_from_str(end_date_string, "%d.%m.%Y") {
+                Ok(date) => end_date = date,
                 Err(e) => {
                     println!("Error parsing start date: {}", e);
                     return;
@@ -68,113 +68,113 @@ fn main() {
             }
 
             // Add Header line to csv output
-            outputFileHandler.add_header_line();
+            output_file_handler.add_header_line();
 
             // Loop through days
-            while currentDate <= endDate {
-                let mut lineToAdd = String::new();
+            while current_date <= end_date {
+                let mut line_to_add = String::new();
 
                 // Get the german name of the weekday
-                let (number, WeekdayName) = get_german_weekday(currentDate.weekday());
-                let currentDateAndWeekdayString =
-                    format_date_string(currentDate) + "," + &WeekdayName;
+                let (_number, weekday_name) = get_german_weekday(current_date.weekday());
+                let current_date_and_weekday_string =
+                    format_date_string(current_date) + "," + &weekday_name;
 
                 // Check for weekends and continue if yes
-                if is_weekend(currentDate) {
-                    currentDate = currentDate.succ_opt().unwrap();
-                    daysWeekendAndHolidays += 1;
+                if is_weekend(current_date) {
+                    current_date = current_date.succ_opt().unwrap();
+                    days_weekend_and_holidays += 1;
                     continue;
                 }
 
                 // Check for global holidays
-                if globalHolidays.contains(&currentDate) {
-                    lineToAdd.push_str(&currentDateAndWeekdayString);
-                    lineToAdd.push_str(&String::from(",Feiertag,"));
-                    outputFileHandler.add_line(&lineToAdd);
-                    currentDate = currentDate.succ_opt().unwrap();
+                if global_holidays.contains(&current_date) {
+                    line_to_add.push_str(&current_date_and_weekday_string);
+                    line_to_add.push_str(&String::from(",Feiertag,"));
+                    output_file_handler.add_line(&line_to_add);
+                    current_date = current_date.succ_opt().unwrap();
                     continue;
                 }
 
                 // Add current date and weekday
-                lineToAdd.push_str(&currentDateAndWeekdayString);
-                lineToAdd.push_str(",");
+                line_to_add.push_str(&current_date_and_weekday_string);
+                line_to_add.push_str(",");
 
                 // Plan morning employee
-                let key = format!("{}v", currentDate.weekday().number_from_monday());
+                let key = format!("{}v", current_date.weekday().number_from_monday());
                 if company_data.fix_days[0].get(&key).unwrap_or(&0) > &0 {
                     let id_to_find = company_data.fix_days[0].get(&key).unwrap_or(&0);
                     if let Some(index) = employees.iter().position(|e| e.id == *id_to_find) {
-                        lineToAdd.push_str(&employees[index].short);
+                        line_to_add.push_str(&employees[index].short);
                         // TODO: move this to a function, including the part in plan_employee
                         employees[index].count =
                             employees[index].count + (1.0 * (1.0 / employees[index].percent));
                         // Set last duty to this date
-                        employees[index].last_duty = currentDate;
+                        employees[index].last_duty = current_date;
                     } else {
                         println!("Employee with ID {} not found", id_to_find);
                     }
                 } else {
-                    let mut employeeShortToPlan = plan_employee(&mut employees, currentDate);
+                    let mut employee_short_to_plan = plan_employee(&mut employees, current_date);
                     let mut attempts = 0;
-                    while employeeShortToPlan == "Error:isOffDay" && attempts < 5000 {
-                        employeeShortToPlan = plan_employee(&mut employees, currentDate);
+                    while employee_short_to_plan == "Error:isOffDay" && attempts < 5000 {
+                        employee_short_to_plan = plan_employee(&mut employees, current_date);
                         attempts += 1;
                     }
-                    if employeeShortToPlan.contains("Error") {
+                    if employee_short_to_plan.contains("Error") {
                         errors += 1;
                     }
-                    lineToAdd.push_str(&employeeShortToPlan);
+                    line_to_add.push_str(&employee_short_to_plan);
                 }
 
                 // Add divider of morning and afternoon (yes, it's just a coma)
-                lineToAdd.push_str(",");
+                line_to_add.push_str(",");
 
                 // Plan afternoon employee
-                let key = format!("{}n", currentDate.weekday().number_from_monday());
+                let key = format!("{}n", current_date.weekday().number_from_monday());
                 if company_data.fix_days[0].get(&key).unwrap_or(&0) > &0 {
                     let id_to_find = company_data.fix_days[0].get(&key).unwrap_or(&0);
                     if let Some(index) = employees.iter().position(|e| e.id == *id_to_find) {
-                        lineToAdd.push_str(&employees[index].short);
+                        line_to_add.push_str(&employees[index].short);
                         // TODO: move this to a function, including the part in plan_employee
                         employees[index].count =
                             employees[index].count + (1.0 * (1.0 / employees[index].percent));
                         // Set last duty to this date
-                        employees[index].last_duty = currentDate;
+                        employees[index].last_duty = current_date;
                     } else {
                         println!("Employee with ID {} not found", id_to_find);
                     }
                 } else {
-                    let mut employeeShortToPlan = plan_employee(&mut employees, currentDate);
+                    let mut employee_short_to_plan = plan_employee(&mut employees, current_date);
                     let mut attempts = 0;
-                    while employeeShortToPlan == "Error:isOffDay" && attempts < 5000 {
-                        employeeShortToPlan = plan_employee(&mut employees, currentDate);
+                    while employee_short_to_plan == "Error:isOffDay" && attempts < 5000 {
+                        employee_short_to_plan = plan_employee(&mut employees, current_date);
                         attempts += 1;
                     }
-                    if employeeShortToPlan.contains("Error") {
+                    if employee_short_to_plan.contains("Error") {
                         errors += 1;
                     }
-                    lineToAdd.push_str(&employeeShortToPlan);
+                    line_to_add.push_str(&employee_short_to_plan);
                 }
 
                 // Add the prepared line to the output
-                outputFileHandler.add_line(&lineToAdd);
+                output_file_handler.add_line(&line_to_add);
 
-                currentDate = currentDate.succ_opt().unwrap();
+                current_date = current_date.succ_opt().unwrap();
 
-                daysPlanned += 1;
+                days_planned += 1;
             }
 
             // Write prepared content to output file
-            let _ = outputFileHandler.write_to_file(&get_file_path(String::from("./output.csv")));
+            let _ = output_file_handler.write_to_file(&get_file_path(String::from("./output.csv")));
 
             println!("\n\nInfo:");
             println!("------------------");
-            println!("Planned from {} to {}", startDate_string, endDate_string);
+            println!("Planned from {} to {}", start_date_string, end_date_string);
             println!(
                 "Weekend- and Holidays: {}",
-                daysWeekendAndHolidays.to_string()
+                days_weekend_and_holidays.to_string()
             );
-            println!("Days planned: {}", daysPlanned.to_string());
+            println!("Days planned: {}", days_planned.to_string());
             println!("Errors: {}", errors.to_string());
 
             println!("\n\nEmployees planned:");
@@ -193,9 +193,9 @@ fn main() {
 
             // Pause for user input
             print!("\n\nPress Enter to continue...");
-            stdout().flush(); // Ensure prompt is visible
+            let _ = stdout().flush(); // Ensure prompt is visible
             let mut input = String::new();
-            stdin().read_line(&mut input); // Wait for Enter
+            let _ = stdin().read_line(&mut input); // Wait for Enter
         }
         Err(e) => println!("Error processing '{}': {}", file_path, e),
     }
@@ -216,29 +216,23 @@ fn plan_employee(employees: &mut Vec<Employee>, date: NaiveDate) -> String {
             .partial_cmp(&b.count)
             .unwrap_or(std::cmp::Ordering::Greater)
     });
-    let mut randomEmployeeNumber = rng.gen_range(0..employees.len() / 2);
-
-    // let mut randomEmployeeNumber = 0;
-
-    // println!("{}", date.format("%d.%m.%Y").to_string());
+    let mut random_employee_number = rng.gen_range(0..employees.len() / 2);
 
     /* Check for last planned duty and sorty by last_duty if too early in the past (2 days). */
     // TODO: make the days configurable
-    if (date - employees[randomEmployeeNumber].last_duty)
+    if (date - employees[random_employee_number].last_duty)
         .num_days()
         .abs()
         < 2
     {
-        // employees.sort_by(|a, b| a.last_duty.cmp(&b.last_duty));
         employees.sort_by(|a, b| a.last_duty.cmp(&b.last_duty));
 
-        // randomEmployeeNumber = rng.gen_range(0..employees.len());
-        randomEmployeeNumber = 0;
+        random_employee_number = 0;
     }
 
     // If the chosen Employee does not work on this day, return an error string
     let weekdaycode = format!("{}v", date.weekday().number_from_monday());
-    if employees[randomEmployeeNumber]
+    if employees[random_employee_number]
         .off_days
         .contains(&weekdaycode)
     {
@@ -246,12 +240,12 @@ fn plan_employee(employees: &mut Vec<Employee>, date: NaiveDate) -> String {
     }
 
     // Add duty to count of employee based on his working percentage
-    employees[randomEmployeeNumber].count = employees[randomEmployeeNumber].count
-        + (1.0 * (1.0 / employees[randomEmployeeNumber].percent));
+    employees[random_employee_number].count = employees[random_employee_number].count
+        + (1.0 * (1.0 / employees[random_employee_number].percent));
     // Set last duty to this date
-    employees[randomEmployeeNumber].last_duty = date;
+    employees[random_employee_number].last_duty = date;
 
-    employees[randomEmployeeNumber].short.clone()
+    employees[random_employee_number].short.clone()
 }
 
 fn parse_json_string(json_str: &str) -> Result<CompanyData, Box<dyn std::error::Error>> {
